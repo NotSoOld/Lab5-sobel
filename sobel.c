149354b33c89259772b99613fb23e881f4060e6f
notsoold <1000lop@gmail.com>
2017-03-31 21:38:23 +0300

    First commit; a lot to do WARNING: stb folder doesn't belong to me, it's from another one's GitHub

---
+++ sobel.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "stb/stb_image.h"
+#include "stb/stb_image_write.h"
+
+int kernelX[3][3] = {{-1, 0, 1},
+					 {-2, 0, 2},
+					 {-1, 0, 1}};
+int kernelY[3][3] = {{ 1, 2, 1},
+					 { 0, 0, 0},
+					 {-1,-2,-1}};
+
+
+char ToGrayscale(char r, char g, char b)
+{
+	
+}
+
+					 
+void main(void)
+{
+	
+	exit(0);
+}
